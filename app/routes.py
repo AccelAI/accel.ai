b55ab0cbe8551ce61224344d86f57d4e29f7e50f
QuickResolve <lauranmontoya@gmail.com>
2016-12-20 16:53:13 -0800

    add routes for blog, curriculum, & workshops

--- app/routes.py
+++ app/routes.py
@@ -4,3 +4,16 @@ from app import app
 @app.route('/index')
 def index():
     return "Accel.AI"
+
+@app.route('/workshops')
+def workshops():
+    return "workshops"
+
+@app.route('/blog')
+def blog():
+    return "blog"
+
+@app.route('/curriculum')
+def curriculum():
+    return "curriculum"
+
