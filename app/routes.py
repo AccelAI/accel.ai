2d97cd4ae769181725db19c5da9959b8f7baadbf
QuickResolve <lauranmontoya@gmail.com>
2016-12-21 00:31:14 -0800

    add beginning of front-end content & styling

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,10 @@
+from flask import *
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Accel.AI"
+    return render_template('index.html', title='Home')
 
 @app.route('/workshops')
 def workshops():
