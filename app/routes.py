7bd739d6af8e7d22c12c00655124b06f4b7396e4
QuickResolve <lauranmontoya@gmail.com>
2016-12-23 15:32:02 -0800

    update site for 1st deployment

--- app/routes.py
+++ app/routes.py
@@ -10,13 +10,19 @@ def index():
 
 @app.route('/workshops')
 def workshops():
-    return "workshops"
+    return render_template('workshops.html', title='Workshops')
 
 @app.route('/blog')
 def blog():
-    return "blog"
+    return render_template('blog.html', title='Blog')
+
+@app.route('/culture')
+def culture():
+      return render_template('culture.html', title='Culture')
 
 @app.route('/curriculum')
 def curriculum():
     return "curriculum"
+
+
 
