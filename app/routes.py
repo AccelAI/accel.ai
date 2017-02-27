cb7ed85e418628abf1180177b45cfc7425e97701
quickresolve <lauranmontoya@gmail.com>
2017-02-26 21:56:45 -0800

    add mailchimp signup page

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
 from flask import *
 from app import app
 
+
+
 @app.route('/')
 @app.route('/index')
 def index():
@@ -14,6 +16,7 @@ def index():
 def blog():
     return render_template('blog.html', title='Blog')
 
+
 @app.route('/culture')
 def culture():
       return render_template('culture.html', title='Culture')
@@ -25,6 +28,10 @@ def curriculum():
 @app.route('/resources')
 def resources():
   return render_template('resources.html', title='Resources')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html', title='Signup')
 
 
 @app.route('/workshops')
